709ead42e25aa471b74e112895b17640e4687841
YowSiaoKang <kangyow4@gmail.com>
2024-03-31 15:16:04 +0800

    Improve code quality

--- src/main/java/seedu/address/ui/AutoCompleteComboBoxPlus.java
+++ src/main/java/seedu/address/ui/AutoCompleteComboBoxPlus.java
@@ -16,114 +16,68 @@ import seedu.address.ui.CommandBox.CommandExecutor;
 public class AutoCompleteComboBoxPlus {
     private static final Logger logger = LogsCenter.getLogger(AutoCompleteComboBoxPlus.class);
 
+    /**
+     * Represents a interface that can compare the typed text with the object to compare.
+     */
     public interface AutoCompleteComparator<T> {
         boolean matches(String typedText, T objectToCompare);
     }
 
-    public static<T> void autoCompleteComboBoxPlus(ComboBox<T> comboBox, AutoCompleteComparator<T> comparatorMethod, CommandExecutor commandExecutor) {
-        ObservableList<T> data = comboBox.getItems();
+    /**
+     * Configures the ComboBox to enable auto-complete.
+     */
+    public static<T> void config(ComboBox<T> comboBox, AutoCompleteComparator<T> comparatorMethod, CommandExecutor commandExecutor) {
+        ObservableList<T> commandHistory = comboBox.getItems();
 
         comboBox.getEditor().focusedProperty().addListener(observable -> {
             if (comboBox.getSelectionModel().getSelectedIndex() < 0) {
-                comboBox.getEditor().setText(null);
+                comboBox.getEditor().setText("");
             }
         });
-        // remove default key listener
         comboBox.addEventHandler(KeyEvent.KEY_PRESSED, t -> comboBox.hide());
-        comboBox.addEventHandler(KeyEvent.KEY_RELEASED, new EventHandler<KeyEvent>() {
+        comboBox.addEventHandler(KeyEvent.KEY_RELEASED, createHandler(comboBox, comparatorMethod, commandHistory, commandExecutor));
+    }
 
-            private boolean moveCaretToPos = false;
-            private int caretPos;
-
+    /**
+     * Creates a EventHandler for the ComboBox.
+     */
+    private static <T> EventHandler<KeyEvent> createHandler(ComboBox<T> comboBox, AutoCompleteComparator<T> comparatorMethod, ObservableList<T> commandHistory,
+     CommandExecutor commandExecutor) {
+        return new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent event) {
                 if (event.getCode() == KeyCode.UP) {
-                    caretPos = -1;
-                    if (comboBox.getEditor().getText() != null) {
-                        moveCaret(comboBox.getEditor().getText().length());
-                    }
+                    processArrowKeys(event, comboBox);
                     return;
                 } else if (event.getCode() == KeyCode.DOWN) {
-                    if (!comboBox.isShowing()) {
-                        comboBox.show();
-                    }
-                    caretPos = -1;
-                    if (comboBox.getEditor().getText() != null) {
-                        moveCaret(comboBox.getEditor().getText().length());
-                    }
+                    processArrowKeys(event, comboBox);
                     return;
                 } else if (event.getCode() == KeyCode.BACK_SPACE) {
-                    if (comboBox.getEditor().getText() != null) {
-                        moveCaretToPos = true;
-                        caretPos = comboBox.getEditor().getCaretPosition();
-                    }
+                    processBackspace(comboBox);
                 } else if (event.getCode() == KeyCode.DELETE) {
-                    if (comboBox.getEditor().getText() != null) {
-                        moveCaretToPos = true;
-                        caretPos = comboBox.getEditor().getCaretPosition();
-                    }
+                    processDelete(comboBox);
                 } else if (event.getCode() == KeyCode.ENTER) {
-                    String commandText = comboBox.getEditor().getText();
-                    if (commandText.equals("")) {
-                        return;
-                    }
-
-                    try {
-                        commandExecutor.execute(commandText);
-                        comboBox.getEditor().setText("");
-                        commandText = commandText.trim();
-                        if (!data.contains(commandText)) {
-                            data.add(0, (T) commandText);
-                        }
-                        comboBox.setItems(data);
-                        comboBox.hide();
-                        return;
-                    } catch (CommandException | ParseException e) {
-                        // setStyleToIndicateCommandFailure();
-                        return;
-                    }
+                    processEnter(comboBox, commandHistory, commandExecutor);
+                    return;
                 }
+
                 comboBox.hide(); // reset the filter size
 
+                // Override the default behaviour of the ComboBox
                 if (event.getCode() == KeyCode.RIGHT || event.getCode() == KeyCode.LEFT || event.getCode().equals(KeyCode.SHIFT) || event.getCode().equals(KeyCode.CONTROL)
                         || event.isControlDown() || event.getCode() == KeyCode.HOME
                         || event.getCode() == KeyCode.END || event.getCode() == KeyCode.TAB) {
                     return;
                 }
 
-                ObservableList<T> list = FXCollections.observableArrayList();
-                for (T aData : data) {
-                    if (aData != null && comboBox.getEditor().getText() != null && comparatorMethod.matches(comboBox.getEditor().getText(), aData)) {
-                        list.add(aData);
-                    }
-                }
-                String t = "";
-                if (comboBox.getEditor().getText() != null) {
-                    t = comboBox.getEditor().getText();
-                }
-
-                comboBox.setItems(list);
-                comboBox.getEditor().setText(t);
-                if (!moveCaretToPos) {
-                    caretPos = -1;
-                }
-                moveCaret(t.length());
-                if (!list.isEmpty()) {
-                    comboBox.show();
-                }
+                processTextInput(comboBox, commandHistory, comparatorMethod);
             }
-
-            private void moveCaret(int textLength) {
-                if (caretPos == -1) {
-                    comboBox.getEditor().positionCaret(textLength);
-                } else {
-                    comboBox.getEditor().positionCaret(caretPos);
-                }
-                moveCaretToPos = false;
-            }
-        });
+        };
     }
 
+    /**
+     * Returns the value of the ComboBox.
+     */
     public static<T> T getComboBoxValue(ComboBox<T> comboBox){
         if (comboBox.getSelectionModel().getSelectedIndex() < 0) {
             return null;
@@ -132,4 +86,106 @@ public class AutoCompleteComboBoxPlus {
         }
     }
 
+    /**
+     * Moves the caret to the specified position.
+     */
+    private static <T> void moveCaret(ComboBox<T> comboBox, int caretPos) {
+        comboBox.getEditor().positionCaret(caretPos);
+    }
+
+    private static <T> void processArrowKeys(KeyEvent event, ComboBox<T> comboBox) {
+        logger.info("Arrow key pressed");
+        if (event.getCode() == KeyCode.UP) {
+            if (comboBox.getEditor().getText() != null) {
+                moveCaret(comboBox, comboBox.getEditor().getText().length());
+            }
+            return;
+        } else if (event.getCode() == KeyCode.DOWN) {
+            if (!comboBox.isShowing()) {
+                comboBox.show();
+            }
+            if (comboBox.getEditor().getText() != null) {
+                moveCaret(comboBox, comboBox.getEditor().getText().length());
+            }
+            return;
+        }
+    }
+
+    private static <T> int processBackspace(ComboBox<T> comboBox) {
+        logger.info("Backspace key pressed");
+        int caretPos = -1;
+        if (comboBox.getEditor().getText() != null) {
+            caretPos = comboBox.getEditor().getCaretPosition();
+        }
+        return caretPos;
+    }
+
+    private static <T> int processDelete(ComboBox<T> comboBox) {
+        logger.info("Delete key pressed");
+        int caretPos = -1;
+        if (comboBox.getEditor().getText() != null) {
+            caretPos = comboBox.getEditor().getCaretPosition();
+        }
+        return caretPos;
+    }
+
+    private static <T> void processEnter(ComboBox<T> comboBox, ObservableList<T> commandHistory, CommandExecutor commandExecutor){
+        String commandText = comboBox.getEditor().getText();
+        if (commandText.equals("")) {
+            return;
+        }
+
+        try {
+            commandExecutor.execute(commandText);
+            comboBox.getEditor().setText("");
+            commandText = commandText.trim();
+            if (commandHistory.contains(commandText)) {
+                commandHistory.remove(commandText);
+            }
+            commandHistory.add(0, (T) commandText);
+            comboBox.setItems(commandHistory);
+            comboBox.hide();
+            return;
+        } catch (CommandException | ParseException e) {
+            setStyleToIndicateCommandFailure(comboBox);
+            return;
+        }
+    }
+
+    public static <T> void processTextInput(ComboBox<T> comboBox, ObservableList<T> commandHistory, AutoCompleteComparator<T> comparatorMethod) {
+        setStyleToDefault(comboBox);
+        ObservableList<T> list = FXCollections.observableArrayList();
+        for (T aData : commandHistory) {
+            if (aData != null && comboBox.getEditor().getText() != null && comparatorMethod.matches(comboBox.getEditor().getText(), aData)) {
+                list.add(aData);
+            }
+        }
+        String t = "";
+        if (comboBox.getEditor().getText() != null) {
+            t = comboBox.getEditor().getText();
+        }
+
+        comboBox.setItems(list);
+        comboBox.getEditor().setText(t);
+        moveCaret(comboBox, t.length());
+        if (!list.isEmpty()) {
+            comboBox.show();
+        }
+    }
+
+     /**
+     * Sets the command box style to indicate a failed command.
+     */
+    public static<T> void setStyleToIndicateCommandFailure(ComboBox<T> comboBox) {
+        comboBox.setStyle("-fx-border-color: red;");
+        comboBox.getEditor().setStyle("-fx-text-fill: red; -fx-background-color: #FFD2D2;");
+    }
+
+    /**
+     * Sets the command box style to use the default style.
+     */
+    public static<T> void setStyleToDefault(ComboBox<T> comboBox) {
+        comboBox.setStyle(null);
+        comboBox.getEditor().setStyle(null);
+    }
 }
--- src/main/java/seedu/address/ui/CommandBox.java
+++ src/main/java/seedu/address/ui/CommandBox.java
@@ -1,19 +1,9 @@
 package seedu.address.ui;
 
-import java.util.List;
-
-import javafx.application.Platform;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.collections.transformation.FilteredList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
-import javafx.scene.control.skin.ComboBoxListViewSkin;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Region;
-import javafx.util.Callback;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -41,66 +31,9 @@ public class CommandBox extends UiPart<Region> {
         commandTextField.setVisibleRowCount(8);
         commandTextField.setItems(FXCollections.observableArrayList("add", "clear", "delete", "edit", "exit", "find", "help", "list"));
         // Set the command box to be auto-completable
-        AutoCompleteComboBoxPlus.autoCompleteComboBoxPlus(commandTextField, (typedText, itemToCompare) -> itemToCompare.toLowerCase().contains(typedText.toLowerCase()) || itemToCompare.toString().equals(typedText), commandExecutor);
-    
-        // calls #setStyleToDefault() whenever there is a change to the text of the command box.
-        // commandTextField.valueProperty().addListener((unused1, unused2, unused3) -> setStyleToDefault());
-        // commandTextField.addEventHandler(KeyEvent.KEY_PRESSED, event -> handleUpDownKeyPressed(event));
+        AutoCompleteComboBoxPlus.config(commandTextField, (typedText, itemToCompare) -> itemToCompare.toLowerCase().contains(typedText.toLowerCase()) || itemToCompare.toString().equals(typedText), commandExecutor);
     }
 
-    /**
-     * Handles the Enter button pressed event.
-     */
-    @FXML
-    private void handleCommandEntered() {
-        String commandText = commandTextField.getValue();
-        if (commandText.equals("")) {
-            return;
-        }
-
-        try {
-            commandExecutor.execute(commandText);
-            commandTextField.setValue("");
-        } catch (CommandException | ParseException e) {
-            setStyleToIndicateCommandFailure();
-        }
-    }
-
-    /**
-     * Sets the command box style to use the default style.
-     */
-    private void setStyleToDefault() {
-        commandTextField.getStyleClass().remove(ERROR_STYLE_CLASS);
-    }
-
-    /**
-     * Sets the command box style to indicate a failed command.
-     */
-    public void setStyleToIndicateCommandFailure() {
-        ObservableList<String> styleClass = commandTextField.getStyleClass();
-
-        if (styleClass.contains(ERROR_STYLE_CLASS)) {
-            return;
-        }
-
-        styleClass.add(ERROR_STYLE_CLASS);
-    }
-
-    /**
-     * Handles the up and down key pressed event.
-     */
-    // private void handleUpDownKeyPressed(KeyEvent event) {
-    //     if (event.getCode().equals(KeyCode.UP)) {
-    //         commandTextField.setValue("up key pressed");
-    //     } else if (event.getCode().equals(KeyCode.DOWN)) {
-    //         commandTextField.setValue("down key pressed");
-    //     }
-    // }
-
-    @FXML
-    private void handleSelection() {
-        commandTextField.getSelectionModel().select(0);
-    }
     /**
      * Represents a function that can execute commands.
      */
@@ -115,34 +48,3 @@ public class CommandBox extends UiPart<Region> {
     }
 
 }
-
-// class AutoShowComboBoxHelper {
-//     public AutoShowComboBoxHelper(final ComboBox<String> comboBox, final Callback<String, String> textBuilder) {
-//         ComboBoxListViewSkin<String> comboBoxListViewSkin = new ComboBoxListViewSkin<>(comboBox);
-//         comboBoxListViewSkin.getPopupContent().addEventFilter(KeyEvent.ANY, (event) -> {
-//             if( event.getCode() == KeyCode.SPACE ) {
-//                 event.consume();
-//             }
-//         });
-//         comboBox.setSkin(comboBoxListViewSkin);
-
-//         final ObservableList<String> items = FXCollections.observableArrayList(comboBox.getItems());
-
-//         comboBox.getEditor().textProperty().addListener((ov, o, n) -> {
-//             if (n.equals(comboBox.getSelectionModel().getSelectedItem())) {
-//                 return;
-//             }
-            
-//             Platform.runLater(() -> comboBox.hide());
-//             final FilteredList<String> filtered = items.filtered(s -> textBuilder.call(s).toLowerCase().contains(n.toLowerCase()));
-//             if (filtered.isEmpty()) {
-//                 Platform.runLater(() -> comboBox.getItems().setAll(items));
-//             } else {
-//                 Platform.runLater(() -> {
-//                     comboBox.getItems().setAll(filtered);
-//                     comboBox.show();
-//                 });
-//             }
-//         });
-//     }
-// }
