1b482e822be6348cb2244a65f5f167495b7aede2
xuwen-ho <ho.xu.wen@gmail.com>
2024-04-01 12:44:23 +0800

    Copy emails to clipboard
    Allow the filtered list of emails to be copied to the clipboard
    
    Previously, the filtered list of emails was only printed to the command line
    
    Lets copy it to to the clipboard so it can be pasted easily

--- src/main/java/seedu/address/logic/commands/CopyCommand.java
+++ src/main/java/seedu/address/logic/commands/CopyCommand.java
@@ -1,8 +1,5 @@
 package seedu.address.logic.commands;
 
-import seedu.address.commons.core.index.Index;
-import seedu.address.commons.util.ToStringBuilder;
-import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
@@ -10,6 +7,9 @@ import seedu.address.model.person.Person;
 import java.util.List;
 
 import static java.util.Objects.requireNonNull;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 
 public class CopyCommand extends Command {
 
@@ -20,20 +20,11 @@ public class CopyCommand extends Command {
 
     public static final String MESSAGE_COPY_SUCCESS = "Emails copied to clipboard";
 
-//    private final Index targetIndex;
-
-//    public CopyCommand(Index targetIndex) {
-//        this.targetIndex = targetIndex;
-//    }
-
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
         List<Person> lastShownList = model.getFilteredPersonList();
-//        Person personToDelete = lastShownList.get(targetIndex.getZeroBased());
-//        model.deletePerson(personToDelete);
 
-//        model.copyEmailsToClipboard();
         copyEmailsToClipboard(lastShownList);
 
         return new CommandResult(String.format(MESSAGE_COPY_SUCCESS));
@@ -54,28 +45,12 @@ public class CopyCommand extends Command {
             sb.delete(strlen - 2, strlen - 1);
         }
 
-        System.out.println(sb.toString());
-    }
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
 
-//    @Override
-//    public boolean equals(Object other) {
-//        if (other == this) {
-//            return true;
-//        }
-//
-//        // instanceof handles nulls
-//        if (!(other instanceof DeleteCommand)) {
-//            return false;
-//        }
-//
-//        DeleteCommand otherDeleteCommand = (DeleteCommand) other;
-//        return targetIndex.equals(otherDeleteCommand.targetIndex);
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return new ToStringBuilder(this)
-//                .add("targetIndex", targetIndex)
-//                .toString();
-//    }
+        // Create a StringSelection object to hold the text
+        StringSelection selection = new StringSelection(sb.toString());
+
+        // Set the contents of the clipboard to the StringSelection
+        clipboard.setContents(selection, null);
+    }
 }
